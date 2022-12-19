5bccc7e82883fd593902942910783f30e477e2d6
Matheus Carvalho <matheus12bh@gmail.com>
2022-12-19 15:17:27 -0800

    create-term-seeder

--- database/seeders/TermSeeder.php
+++ database/seeders/TermSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Term;
 
 class TermSeeder extends Seeder
 {
@@ -13,6 +14,32 @@ class TermSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Term::create([
+            'name' => 'Fall 2022',
+            'start_date' => '2022-08-28',
+            'end_date' => '2022-12-20',
+            'status' => 2
+        ]);
+
+        Term::create([
+            'name' => 'Spring 2023',
+            'start_date' => '2022-01-17',
+            'end_date' => '2022-12-20',
+            'status' => 1
+        ]);
+
+        Term::create([
+            'name' => 'Fall 2023',
+            'start_date' => '2022-08-28',
+            'end_date' => '2022-12-16',
+            'status' => 0
+        ]);
+
+        Term::create([
+            'name' => 'Spring 2024',
+            'start_date' => '2022-01-16',
+            'end_date' => '2022-05-11',
+            'status' => 0
+        ]);
     }
 }
