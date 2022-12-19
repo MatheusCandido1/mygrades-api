d7b4a208d9781518b6ee4627b273eadb35c5a6d7
Matheus Carvalho <matheus12bh@gmail.com>
2022-12-19 15:02:31 -0800

    add-terms-migration

---
+++ database/migrations/2022_12_19_230145_create_terms_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTermsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('terms', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->datetime('start_date');
+            $table->datetime('end_date');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('terms');
+    }
+}
