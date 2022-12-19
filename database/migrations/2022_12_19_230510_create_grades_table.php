c65297348bce25f9ec8c486e752d1349f3732b5a
Matheus Carvalho <matheus12bh@gmail.com>
2022-12-19 15:06:57 -0800

    add-grades-migration

---
+++ database/migrations/2022_12_19_230510_create_grades_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGradesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('grades', function (Blueprint $table) {
+            $table->id();
+            $table->string('grade');
+            $table->bigUnsignedInteger('term_id');
+            $table->bigUnsignedInteger('course_id');
+            $table->bigUnsignedInteger('user_id');
+            $table->foreign('term_id')->references('id')->on('terms');
+            $table->foreign('course_id')->references('id')->on('courses');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('grades');
+    }
+}
